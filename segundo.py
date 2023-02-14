97c5a060e7dd536018dfacf1d441a907add37dc4
hanz0x1 <59904811+hanz0x1@users.noreply.github.com>
2023-02-13 18:45:28 -0600

    Add files via upload

---
+++ segundo.py
@@ -0,0 +1,27 @@
+a = 2
+print(type(a))
+print (a)
+
+b = 9223372036854775807
+print(type(b))
+print (b)
+
+pi = 3.14
+print(type(pi))
+print (pi)
+
+c = 'A'
+print(type(c))
+print (c)
+
+name = 'John Doe'
+print(type(name))
+print (name)
+
+q = True
+print(type(q))
+print (q)
+
+x = None
+print(type(x))
+print (x)
